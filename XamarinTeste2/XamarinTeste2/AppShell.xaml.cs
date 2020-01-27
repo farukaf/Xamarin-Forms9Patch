b07df27a4d7f2187de81fb644bd2644c26e55d0b
Faruk Abdo Feres <faruk_feres@hotmail.com>
2020-01-27 08:26:55 -0200

    Initial commit

---
+++ XamarinTeste2/XamarinTeste2/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinTeste2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
